99e1d2e3b9aadddd9414167b8d054e5dedc33750
yuragethigh <125820956+yuragethigh@users.noreply.github.com>
2024-07-26 11:31:14 +0300

    Initial Commit

---
+++ FloatingSheet/FloatingSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingSheetApp.swift
+//  FloatingSheet
+//
+//  Created by Yuriy on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FloatingSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
