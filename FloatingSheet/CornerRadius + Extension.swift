66a84b4b6522e469681316d284214eee9c4ea2c2
yuragethigh <yourselfmind@yandex.ru>
2024-09-12 14:06:04 +0300

    d

---
+++ FloatingSheet/CornerRadius + Extension.swift
@@ -0,0 +1,37 @@
+//
+//  Extension.swift
+//  FloatingSheet
+//
+//  Created by Yuriy on 26.07.2024.
+//
+
+import SwiftUI
+
+fileprivate struct CornerRadiusStyle: ViewModifier {
+    
+    var radius: CGFloat
+    var corners: UIRectCorner
+    
+   private struct CornerRadiusShape: Shape {
+
+        var radius = CGFloat.infinity
+        var corners = UIRectCorner.allCorners
+
+        func path(in rect: CGRect) -> Path {
+            let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            return Path(path.cgPath)
+        }
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .clipShape(CornerRadiusShape(radius: radius, corners: corners))
+    }
+}
+
+internal extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        ModifiedContent(content: self, modifier: CornerRadiusStyle(radius: radius, corners: corners))
+    }
+}
+
